d63b5fa317238d771a271d80b59543a22afdaa59
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-01-25 23:07:35 +0500

    Rules_over

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Components/Header.jsx";
 import Footer from "./Components/Footer.jsx";
+import MainContent from "./Components/MainContent.jsx";
 
 const App = () => {
     return (
         <div>
             <Header/>
+            <MainContent/>
             <Footer/>
         </div>
 
---
+++ src/Components/MainContent.jsx
@@ -0,0 +1,13 @@
+const MainContent = () => {
+    return (
+        <div style={{background:'beige'}}>
+            <h1 style={{textAlign:'center'}}>Resolutions</h1>
+            <p className={'Quote'} style={{marginTop:'10px'}}>Whether you want to make small
+                improvements to your lifestyle or treat yourself
+                to some useful knowledge, the Harvard community
+                can help you start something new this year.</p>
+            <button style={{color:'red'}}>Apply now</button>
+        </div>
+    )
+}
+export default MainContent
